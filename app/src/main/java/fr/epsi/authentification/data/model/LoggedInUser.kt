b5e3a56fbd21ab2c21d596d5590b169c13b73a88
Fulgryn <gabdu74@gmail.com>
2021-11-24 09:49:34 +0100

    first commit

---
+++ app/src/main/java/fr/epsi/authentification/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package fr.epsi.authentification.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ app/src/main/java/fr/epsi/authentification/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package fr.epsi.authentification.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
